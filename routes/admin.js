cf957c8e2039e213e289b14936d4d74f08e1db90
Sam Machin <sam@sammachin.com>
2022-01-14 10:35:42 +0000

    WIP

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var mustache = require('mustache');
 
 var events = require("../lib/events");
 var templates = require("../lib/templates");
+const settings = require("../config");
 
 var app = express();
 app.get("/admin/log",function(req,res) {
@@ -10,6 +11,24 @@ app.get("/admin/log",function(req,res) {
     context.sessionuser = req.session.user;
     events.get().then(function(events) {
         context.events = events;
+        res.send(mustache.render(templates.events,context,templates.partials));
+    }).catch(function(err) {
+        console.log(err);
+        context.err = err;
+        context.events = [];
+        res.send(mustache.render(templates.events,context,templates.partials));
+    });
+});
+
+
+
+app.get("/admin/node/delete",function(req,res) {
+    var context = {};
+    context.sessionuser = req.session.user;
+    console.log(context.sessionuser)
+    if (context.sessionuser){
+    } else{
+    }
         res.send(mustache.render(templates.events,context,templates.partials));
     }).catch(function(err) {
         console.log(err);
